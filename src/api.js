75fd2f599bf397ddf9526df0e1a7a87481e5ed8e
Neelabh Singh <neelabh041997@gmail.com>
2022-10-10 10:36:03 +0530

    City Search Component

---
+++ src/api.js
@@ -0,0 +1,10 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '12559e7048msh139e01598805773p1ca9f8jsne397225beb00',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
